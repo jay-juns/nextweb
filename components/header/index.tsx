a2b9cfe2ebccbf86795fe59312568416f9c4d7cd
jay-juns <necrozerg2@naver.com>
2022-03-14 10:48:51 +0900

    pc, mobile 쿼리 추가

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,74 +1,98 @@
 import Link from "next/link"
 import { useRouter } from "next/router";
 import { signOut, useSession } from "next-auth/react";
+import useMediaQuery from "../../utils/customHooks/useMediaQuery";
+import { Icon } from '@iconify/react';
 
 const Header = ({ children }: any) => {
   const router = useRouter();
   const theme = localStorage.getItem('theme');
   const { data: session, status } = useSession();
+  const [useQuery] = useMediaQuery('screen and (min-width: 767px)');
   console.log(session, 'session')
   
   return (
     <header className="header">
       <nav className="header-nav">
-        <div className="left-nav">
-          <Link href="/">
-            <a className="home-logo-image">
-              {theme === 'dark' ?
-               <img src="/images/logo/logo-d.png" alt="logo" />
-                :
-                <img src="/images/logo/logo-w.png" alt="logo" /> 
-              }
-            </a>
-          </Link>
-          <ul>
-            <li className={router.asPath === "/about" ? "active" : ""}>
-              <Link href="/about">
-                <a>About</a>
+        {useQuery ?
+            <>
+            <div className="left-nav">
+              <Link href="/">
+                <a className="home-logo-image">
+                  {theme === 'dark' ?
+                  <img src="/images/logo/logo-d.png" alt="logo" />
+                    :
+                    <img src="/images/logo/logo-w.png" alt="logo" /> 
+                  }
+                </a>
               </Link>
-            </li>
-            <li className={router.asPath === "/contents" ? "active" : ""}>
-              <Link href="/contents">
-                <a>Contents</a>
-              </Link>
-            </li>
-            <li className={router.asPath === "/contact_us" ? "active" : ""}>
-              <Link href="/contact_us">
-                <a>Contact Us</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="right-nav">
-              {status === 'authenticated' ?
-              (
-                <div>
-                  <span className={router.asPath === "/dashboard" ? "active" : ""}>
-                    <Link href="/dashboard">
-                    {session?.user?.name}
-                    </Link>
-                  </span>
-                  <button className="btn sign-out--btn" onClick={()=> signOut()}>Log out</button>
-                </div>
-              ) 
-              : 
-              (
-                <>
-                  <span className={router.asPath === "/login" ? "active" : ""}>
-                    <Link href="/login">
-                      Log In
-                    </Link>
-                  </span>
-                  <span className={router.asPath === "/signup" ? "active" : ""}>
-                    <Link href="/signup">
-                      Sign Up
-                    </Link>
-                  </span>
-                </>
-              )}
-          {children && children}
-        </div>
-      </nav>
+              <ul>
+                <li className={router.asPath === "/about" ? "active" : ""}>
+                  <Link href="/about">
+                    <a>About</a>
+                  </Link>
+                </li>
+                <li className={router.asPath === "/contents" ? "active" : ""}>
+                  <Link href="/contents">
+                    <a>Contents</a>
+                  </Link>
+                </li>
+                <li className={router.asPath === "/contact_us" ? "active" : ""}>
+                  <Link href="/contact_us">
+                    <a>Contact Us</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="right-nav">
+                  {status === 'authenticated' ?
+                  (
+                    <div>
+                      <span className={router.asPath === "/dashboard" ? "active" : ""}>
+                        <Link href="/dashboard">
+                        {session?.user?.name}
+                        </Link>
+                      </span>
+                      <button className="btn sign-out--btn" onClick={()=> signOut()}>Log out</button>
+                    </div>
+                  ) 
+                  : 
+                  (
+                    <>
+                      <span className={router.asPath === "/login" ? "active" : ""}>
+                        <Link href="/login">
+                          Log In
+                        </Link>
+                      </span>
+                      <span className={router.asPath === "/signup" ? "active" : ""}>
+                        <Link href="/signup">
+                          Sign Up
+                        </Link>
+                      </span>
+                    </>
+                  )}
+              {children && children}
+            </div>   
+            </>
+            :
+            <div className="mobile-nav-menu">
+              <button className="btn">
+                <Icon className="menu" icon="bx:menu" />    
+              </button>
+              <div className="mobile-nav-logo--area">
+                <Link href="/">
+                  <a className="home-logo-image">
+                    {theme === 'dark' ?
+                    <img src="/images/logo/logo-d.png" alt="logo" />
+                      :
+                      <img src="/images/logo/logo-w.png" alt="logo" /> 
+                    }
+                  </a>
+                </Link>
+              </div>
+            </div>
+        }
+      </nav> 
     </header>
   )
 }
